7c442cf4ceb567a7170b29734927f746f239042d
Chris Northcott <cmn@cmn.me.uk>
2015-06-12 18:22:06 +0100

    Initialise/open the given RTLSDR (03).

--- rtl-sdr-demo.c
+++ rtl-sdr-demo.c
@@ -8,10 +8,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 #include <rtl-sdr.h>
+
+#define USE_DEVICE 0
 
 uint32_t count;
 char vid[256], pid[256], ser[256];
+rtlsdr_dev_t *device;
 
 void init_rtlsdr()
 {
@@ -29,8 +33,16 @@ void init_rtlsdr()
 	for(int i = 0; i < count; i++)
 	{
 		rtlsdr_get_device_usb_strings(i, vid, pid, ser);
-		fprintf(stdout, "#%d\t%s\t%s\t%d\n",
-			i, vid, pid, ser);
+		char selected = (i == USE_DEVICE)? '*' : ' ';
+		fprintf(stdout, "#%d%c\t%s\t%s\t%d\n",
+			i, selected, vid, pid, ser);
+	}
+
+	if(rtlsdr_open(&device, USE_DEVICE) < 0)
+	{
+		fprintf(stderr, "Couldn't open RTLSDR #%d: %s (%d)\n",
+			USE_DEVICE, strerror(errno), errno);
+		exit(-1);
 	}
 }
 
